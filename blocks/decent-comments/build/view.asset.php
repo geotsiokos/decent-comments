2e950e74e301894dd266dc7e6fabd1a58be9ea4b
George <gtsiokos@gmail.com>
2026-01-12 20:05:45 +0200

    updated build files

--- blocks/decent-comments/build/view.asset.php
+++ blocks/decent-comments/build/view.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-dom', 'react-jsx-runtime', 'wp-api-fetch', 'wp-i18n'), 'version' => 'b482cbb057d63bb32bbd');
+<?php return array('dependencies' => array('react-dom', 'react-jsx-runtime', 'wp-api-fetch', 'wp-i18n'), 'version' => 'f3f4cce029310e10939d');
